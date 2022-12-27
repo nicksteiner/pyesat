5fbe8dce5f695b7a2e27e6df27fd43cd5ca7b1dc
N Steiner <nick.steiner@gmail.com>
2022-12-27 15:46:10 -0500

    Adding setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pyesat',
+    version='0.1.0',
+    author='Nick Steiner',
+    author_email='nick@nsteiner.com',
+    url='https://github.com/nicksteiner/pyesat',
+    description='A Python library for accessing satellite data',
+    packages=['pyesat'],
+    install_requires=[
+        'requests>=2.24.0',
+        'lxml>=4.6.1'
+    ]
+)
